c0d72dc5306767339c359cac1ed2fa76d595ccac
Morten Bøgeskov <mb@dbc.dk>
2021-01-11 08:02:11 +0100

    Deprecated assertThat()

--- src/test/java/dk/dbc/inserts/ArgumentsTest.java
+++ src/test/java/dk/dbc/inserts/ArgumentsTest.java
@@ -22,7 +22,7 @@ import dk.dbc.ExitException;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  *
